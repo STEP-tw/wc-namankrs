5c0bcaa837267c73b67994a461f1cec55f9b688e
naman sharma <44020825+namankrs@users.noreply.github.com>
2018-12-26 14:26:27 +0530

    extracted constants into constants.js

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const NEW_LINE = "\n";
+const TAB = "\t";
+const ENCODING = "utf8";
+
+module.exports = { NEW_LINE, TAB, ENCODING };
--- src/fileUtil.js
+++ src/fileUtil.js
@@ -1,9 +1,11 @@
+const { NEW_LINE, TAB, ENCODING } = require("./constants");
+
 const getCount = function(delimeter, content) {
   return content.split(delimeter).length;
 };
 
 const countCharacters = getCount.bind(null, "");
-const countLines = getCount.bind(null, "\n");
+const countLines = getCount.bind(null, NEW_LINE);
 
 const countWords = function(content) {
   let words = content.trim();
@@ -12,11 +14,11 @@ const countWords = function(content) {
 };
 
 const wc = function([filePath], fs) {
-  let content = fs.readFileSync(filePath, "utf8");
+  let content = fs.readFileSync(filePath, ENCODING);
   let characterCount = countCharacters(content);
   let wordCount = countWords(content);
   let lineCount = countLines(content) - 1;
-  return [lineCount, wordCount, characterCount, filePath].join("\t");
+  return [lineCount, wordCount, characterCount, filePath].join(TAB);
 };
 
 module.exports = { wc };
